f4978b05537a176f6ca0a134869ff54a36775379
wilson chau <a1995060569@gmail.com>
2023-07-12 13:32:34 +0800

    push code

---
+++ src/assets/index.js
@@ -0,0 +1,11 @@
+const assets = {
+  images: {
+    logoDark: require("./images/logo-dark3.png"),
+    appIcon: require("./images/favicon.png"),
+  },
+  colors: {
+    secondary: "#292929",
+  },
+};
+
+export default assets;
